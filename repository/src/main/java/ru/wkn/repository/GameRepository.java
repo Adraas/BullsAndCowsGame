64b73e67d9bc81df3bb21a3492ba25ba45316f1f
Adraas <pickalov.artyom@yandex.ru>
2019-08-08 17:51:54 +0400

    docs: add documentation for the repository interfaces

--- repository/src/main/java/ru/wkn/repository/GameRepository.java
+++ repository/src/main/java/ru/wkn/repository/GameRepository.java
@@ -6,7 +6,20 @@ import ru.wkn.entities.Gamer;
 
 import java.util.Collection;
 
+/**
+ * The interface {@code GameRepository} represents an abstract repository with the specific Spring Data JPA style.
+ * This interface provides customized functional for a work with a datasource of {@code Game} type entities. It's the
+ * DAO layer of the repository.
+ *
+ * @author Orin Adraas
+ */
 public interface GameRepository extends PagingAndSortingRepository<Game, Long> {
 
+    /**
+     * The method for the finding games ({@code Game} type) by the given gamer ({@code Gamer} type) data.
+     *
+     * @param gamer the given gamer data for the searching
+     * @return a collection of found {@code Game} objects
+     */
     Collection<Game> findGamesByGamer(Gamer gamer);
 }
--- repository/src/main/java/ru/wkn/repository/GamerRepository.java
+++ repository/src/main/java/ru/wkn/repository/GamerRepository.java
@@ -3,7 +3,21 @@ package ru.wkn.repository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.wkn.entities.Gamer;
 
+/**
+ * The interface {@code GamerRepository} represents an abstract repository with the specific Spring Data JPA style.
+ * This interface provides customized functional for a work with a datasource of {@code Gamer} type entities. It's the
+ * DAO layer of the repository.
+ *
+ * @author Orin Adraas
+ */
 public interface GamerRepository extends PagingAndSortingRepository<Gamer, Long> {
 
+    /**
+     * The method for the finding gamer ({@code Gamer} type) by the given email and password (authentication process).
+     *
+     * @param email    the given email of a gamer account
+     * @param password the given password of a gamer account
+     * @return a found gamer data as a {@code Gamer} object
+     */
     Gamer findByEmailAndPassword(String email, String password);
 }
