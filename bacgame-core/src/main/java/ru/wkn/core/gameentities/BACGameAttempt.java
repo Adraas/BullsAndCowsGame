8338ced788dc14cad797748905e2f1a5890b9b6b
Adraas <pickalov.artyom@yandex.ru>
2019-08-09 17:09:40 +0400

    feat: add entity for this module

---
+++ bacgame-core/src/main/java/ru/wkn/core/gameentities/BACGameAttempt.java
@@ -0,0 +1,24 @@
+package ru.wkn.core.gameentities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * The class {@code BACGameAttempt} represents a game attempt to guess a hidden number and a grade of an attempt.
+ *
+ * @author Orin Adraas
+ */
+@AllArgsConstructor
+@Getter
+public class BACGameAttempt {
+
+    /**
+     * The input guessed numbers as the {@code String} object.
+     */
+    private String inputNumbersLine;
+
+    /**
+     * The grade of an attempt to guess a hidden number.
+     */
+    private String resultLine;
+}
