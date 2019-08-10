549fb48339be191e67b0fae37f636de94fcf9517
Adraas <pickalov.artyom@yandex.ru>
2019-08-10 12:45:41 +0400

    feat: change testing principle

--- bacgame-core/src/test/java/ru/wkn/core/gamealgorithms/GameAlgorithmsTest.java
+++ bacgame-core/src/test/java/ru/wkn/core/gamealgorithms/GameAlgorithmsTest.java
@@ -4,27 +4,25 @@ import org.junit.jupiter.api.Test;
 import ru.wkn.core.gameexceptions.InputNumbersLineFormatException;
 import ru.wkn.core.gameexceptions.NumbersLineLengthException;
 
-import java.util.regex.Pattern;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GameAlgorithmsTest {
 
     @Test
-    void testCalculatingToCorrectData() throws NumbersLineLengthException, InputNumbersLineFormatException {
+    void testCalculatingToCorrectData() throws NumbersLineLengthException, InputNumbersLineFormatException,
+            NoSuchFieldException, IllegalAccessException {
         GameAlgorithms gameAlgorithms = new GameAlgorithms((byte) 4);
         String hiddenNumbersLine = "1234";
-        assertEquals("4B0C", gameAlgorithms.calculate(hiddenNumbersLine, hiddenNumbersLine).getResultLine());
+        gameAlgorithms.getClass().getField("hiddenNumbersLine").set(gameAlgorithms, hiddenNumbersLine);
+        assertEquals("4B0C", gameAlgorithms.calculate(hiddenNumbersLine).getResultLine());
     }
 
     @Test
     void testCalculatingToIncorrectData() throws NumbersLineLengthException, InputNumbersLineFormatException {
         GameAlgorithms gameAlgorithms = new GameAlgorithms((byte) 4);
-        String hiddenNumbersLine = "1234";
         String inputNumbersLine = "1253";
-        assertEquals("2B1C", gameAlgorithms.calculate(inputNumbersLine, hiddenNumbersLine).getResultLine());
+        assertEquals("2B1C", gameAlgorithms.calculate(inputNumbersLine).getResultLine());
     }
 
     @Test
@@ -35,16 +33,6 @@ class GameAlgorithmsTest {
     @Test
     void testCalculatingToIncorrectInputNumbersLineFormat() {
         assertThrows(InputNumbersLineFormatException.class,
-                () -> new GameAlgorithms((byte) 4).calculate("hello", "world"));
-    }
-
-    @Test
-    void testToRandomValueGenerating() throws NumbersLineLengthException {
-        byte numbersLineLength = 4;
-        GameAlgorithms gameAlgorithms = new GameAlgorithms(numbersLineLength);
-        String hiddenLineFormat = "[0-9]{".concat(String.valueOf(numbersLineLength)).concat("}");
-        String hiddenNumbersLine = gameAlgorithms.generateRandomValue();
-        Pattern pattern = Pattern.compile(hiddenLineFormat);
-        assertTrue(pattern.matcher(hiddenNumbersLine).matches());
+                () -> new GameAlgorithms((byte) 4).calculate("hello world"));
     }
 }
