05a0d0f6359aec977e4cb22f5eb09fb157c48cc3
Adraas <pickalov.artyom@yandex.ru>
2019-08-11 11:46:45 +0400

    docs: add documentation for the result checker

--- bacgame-core/src/main/java/ru/wkn/core/gamealgorithms/GameAttemptResultChecker.java
+++ bacgame-core/src/main/java/ru/wkn/core/gamealgorithms/GameAttemptResultChecker.java
@@ -2,8 +2,19 @@ package ru.wkn.core.gamealgorithms;
 
 import ru.wkn.core.gameentities.GameAttempt;
 
+/**
+ * The class {@code GameAttemptResultChecker} represents the result checker for a data obtained from general algorithms.
+ *
+ * @author Orin Adraas
+ */
 public class GameAttemptResultChecker {
 
+    /**
+     * The method for the grading a data obtained from general algorithms.
+     *
+     * @param gameAttempt the answer of the general algorithms of the game
+     * @return {@code true} if the game over, else - {@code false}
+     */
     public boolean gradeResult(GameAttempt gameAttempt) {
         return false;
     }
