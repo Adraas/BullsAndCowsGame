21bc3775e4d3592ca7386dba8c5e51d9a7eca4c5
Adraas <pickalov.artyom@yandex.ru>
2019-08-17 21:41:19 +0400

    refactor: change registration client-side protocol

--- bacgame-web-core/src/main/webapp/js/Entrance.js
+++ bacgame-web-core/src/main/webapp/js/Entrance.js
@@ -1,26 +1,11 @@
 class Entrance {
 
-    static signIn(loginElement, passwordElement) {
-        if (this.isCorrect([loginElement, passwordElement])) {
-            let login = document.getElementById(loginElement);
-            let password = document.getElementById(passwordElement);
-            let jsonData = "Basic " + btoa(login.value + ", " + password.value);
-            Entrance.doRequest(jsonData, "Authorization", "/bacgame/sign_in", "GET");
-        } else {
-            alert("Проверьте данные!")
-        }
-    }
-
     static signUp(nicknameElement, loginElement, passwordElement) {
-        if (this.isCorrect([loginElement, passwordElement])) {
-            let nickname = document.getElementById(nicknameElement);
-            let login = document.getElementById(loginElement);
-            let password = document.getElementById(passwordElement);
-            let data = "nickname=" + nickname.value + "&login=" + login.value + "&password=" + password.value;
-            Entrance.doRequest(data, null, "/bacgame/sign_up", "POST");
-        } else {
-            alert("Проверьте данные!")
-        }
+        let nickname = document.getElementById(nicknameElement);
+        let login = document.getElementById(loginElement);
+        let password = document.getElementById(passwordElement);
+        let data = "nickname=" + nickname.value + "&login=" + login.value + "&password=" + password.value;
+        Entrance.doRequest(data, null, "/bacgame_sign_up", "POST");
     }
 
     static doRequest(data, header, URL, requestType) {
@@ -28,30 +13,16 @@ class Entrance {
         xmlHttp.onload = function () {
             let response = xmlHttp.responseText;
             if (response.trim() !== "") {
-                if (!response.startsWith("<!")) {
-                    alert(response);
-                } else {
-                    window.document.writeln(response);
-                }
+                alert(response);
             }
         };
         xmlHttp.open(requestType, URL, false);
-        xmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+        BACGame.xmlHttp.overrideMimeType("application/json");
         if (header == null) {
             xmlHttp.send(data);
         } else {
             xmlHttp.setRequestHeader(header, data);
             xmlHttp.send();
         }
-    }
-
-    static isCorrect(elements) {
-        for (let i = 0; i < elements.length; i++) {
-            let isEmpty = document.getElementById(elements[i]).value.trim() === "";
-            if (isEmpty) {
-                return false;
-            }
-        }
-        return true;
     }
 }
