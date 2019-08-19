db059b4467ba8d4bb1d0ae5e0ba2b2f9dd6ed67c
Adraas <pickalov.artyom@yandex.ru>
2019-08-19 11:13:01 +0400

    refactor: change data type to JSON

--- bacgame-web-core/src/main/webapp/js/Entrance.js
+++ bacgame-web-core/src/main/webapp/js/Entrance.js
@@ -1,14 +1,14 @@
 class Entrance {
 
     static signUp(nicknameElement, loginElement, passwordElement) {
-        let nickname = document.getElementById(nicknameElement);
-        let login = document.getElementById(loginElement);
-        let password = document.getElementById(passwordElement);
-        let data = "nickname=" + nickname.value + "&login=" + login.value + "&password=" + password.value;
-        Entrance.doRequest(data, null, "/bacgame_sign_up", "POST");
+        let nickname = document.getElementById(nicknameElement).value;
+        let login = document.getElementById(loginElement).value;
+        let password = document.getElementById(passwordElement).value;
+        let jsonData = JSON.stringify({"nickname": nickname, "login": login, "password": password});
+        Entrance.doRequest(jsonData, null, "/bacgame_sign_up", "POST");
     }
 
-    static doRequest(data, header, URL, requestType) {
+    static doRequest(jsonData, header, URL, requestType) {
         let xmlHttp = new XMLHttpRequest();
         xmlHttp.onload = function () {
             let response = xmlHttp.responseText;
@@ -19,9 +19,9 @@ class Entrance {
         xmlHttp.open(requestType, URL, false);
         BACGame.xmlHttp.overrideMimeType("application/json");
         if (header == null) {
-            xmlHttp.send(data);
+            xmlHttp.send(jsonData);
         } else {
-            xmlHttp.setRequestHeader(header, data);
+            xmlHttp.setRequestHeader(header, jsonData);
             xmlHttp.send();
         }
     }
