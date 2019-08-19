928d6c1967907028ce765e843def1500f4f0ae7f
Adraas <pickalov.artyom@yandex.ru>
2019-08-19 16:43:55 +0400

    refactor: change input parameter type Now this type as the JSONObject.

--- bacgame-web-core/src/main/java/ru/wkn/controllers/EntranceBACGameController.java
+++ bacgame-web-core/src/main/java/ru/wkn/controllers/EntranceBACGameController.java
@@ -1,12 +1,12 @@
 package ru.wkn.controllers;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.wkn.RepositoryFacade;
-import ru.wkn.controllers.json.RegistrationJSONData;
 import ru.wkn.entities.Gamer;
 import ru.wkn.services.GamerService;
 
@@ -19,15 +19,16 @@ public class EntranceBACGameController {
         repositoryFacade = new RepositoryFacade("gamerService");
     }
 
-    @RequestMapping(path = {"/login*"}, method = RequestMethod.GET)
+    @RequestMapping(path = {"/login*"}, method = {RequestMethod.GET})
     public String logIn(@RequestParam(name = "login") String login, @RequestParam(name = "password") String password) {
         Gamer gamer = ((GamerService) repositoryFacade.getService()).authenticate(login, password);
         return gamer != null ? "/secured/bacgame" : "/sign_in";
     }
 
-    @RequestMapping(path = {"/bacgame_sign_up*"}, method = RequestMethod.POST, produces = "application/json")
-    public String registry(@RequestBody RegistrationJSONData data) {
-        Gamer gamer = new Gamer(data.getNickname(), data.getLogin(), data.getPassword());
+    @RequestMapping(path = {"/bacgame_sign_up*"}, method = {RequestMethod.POST}, produces = {"application/json"})
+    public String registry(@RequestBody JSONObject jsonObject) {
+        Gamer gamer = new Gamer(jsonObject.getString("nickname"),
+                jsonObject.getString("login"), jsonObject.getString("password"));
         return ((GamerService) repositoryFacade.getService()).registryNewGamer(gamer) == null ? "/sign_up" : "/sign_in";
     }
 }
