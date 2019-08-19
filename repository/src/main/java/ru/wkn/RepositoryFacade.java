e61a658ffbe3cffd12bdbafa3163ca1bcec64b09
Adraas <pickalov.artyom@yandex.ru>
2019-08-19 10:53:12 +0400

    docs: add documentation for the repository facade

--- repository/src/main/java/ru/wkn/RepositoryFacade.java
+++ repository/src/main/java/ru/wkn/RepositoryFacade.java
@@ -5,12 +5,26 @@ import lombok.Setter;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.wkn.service.IService;
 
+/**
+ * The class {@code RepositoryFacade} represents the facade of the repository.
+ *
+ * @author Orin Adraas
+ */
 @Getter
 @Setter
 public class RepositoryFacade {
 
+    /**
+     * The abstract representation of the repository service.
+     */
     private IService service;
 
+    /**
+     * Initializes a newly created {@code RepositoryFacade} object with the assignment value from the context for the
+     * {@code service} property.
+     *
+     * @param serviceBeanName the repository service bean name
+     */
     public RepositoryFacade(String serviceBeanName) {
         service = (IService) new ClassPathXmlApplicationContext("/META-INF/spring-data-context.xml")
                 .getBean(serviceBeanName);
