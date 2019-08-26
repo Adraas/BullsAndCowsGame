a634557b1cac1b9c1560699f686e15515aa2c4d8
Adraas <pickalov.artyom@yandex.ru>
2019-08-26 19:42:23 +0400

    refactor: change answer for invalid input data

--- bacgame-web-core/src/main/java/ru/wkn/authentication/providers/CustomSimpleAuthenticationProvider.java
+++ bacgame-web-core/src/main/java/ru/wkn/authentication/providers/CustomSimpleAuthenticationProvider.java
@@ -1,6 +1,7 @@
 package ru.wkn.authentication.providers;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -21,7 +22,7 @@ public class CustomSimpleAuthenticationProvider implements AuthenticationProvide
         if (gamer != null) {
             return new UsernamePasswordAuthenticationToken(login, password, new ArrayList<>());
         }
-        return null;
+        throw new BadCredentialsException("login or password is invalid");
     }
 
     @Override
